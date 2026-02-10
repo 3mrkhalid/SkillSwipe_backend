2bff0dab073b928022fecb9dd5e63a3d72ba9440
3mrkhalid <amrahmedyoussef100@gmail.com>
2026-02-11 00:42:07 +0200

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://3mrkhalid.github.io/SkillSwipe_Frontend'
+    'https://3mrkhalid.github.io'
 ];
 
 module.exports = allowedOrigins;
